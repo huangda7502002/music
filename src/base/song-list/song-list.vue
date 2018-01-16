22fdc1ac3cda2b61868fe800b964914ab9979ddb
haungda <353656864@qq.com>
2018-01-16 12:28:42 +0800

    歌手详情

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li v-for="song in songs" class="item">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc"></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    props: {
+      songs: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      getDesc (song) {
+        return `${song.singer}`
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,8 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-
-    </div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage" ></music-list>
   </transition>
 </template>
 
@@ -10,12 +8,25 @@
   import {mapGetters} from 'vuex'
   import {getSingerDetail} from '@/api/singer'
   import {ERR_OK} from '@/api/config'
+  import {createSong} from '@/common/js/song'
+  import MusicList from '@/components/music-list/music-list'
 
   export default {
+    components: {
+      MusicList
+    },
     data () {
-      return {}
+      return {
+        songs: []
+      }
     },
     computed: {
+      title () {
+        return this.singer.name
+      },
+      bgImage () {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -32,9 +43,21 @@
 
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res)
+//            console.log(res)
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
           }
         })
+      },
+      _normalizeSongs (list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
     }
   }
